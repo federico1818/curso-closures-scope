96308f33f9b8c3c672f9d21201b8d9d059ebaacc
Federico González <federico1818@gmail.com>
2023-03-06 00:16:32 -0300

    function scope

---
+++ src/02-function.js
@@ -0,0 +1,10 @@
+function greeting() {
+    let username = 'James'
+    console.log(username)
+    if(username === 'James') {
+        console.log(`Hello, ${ username }!`)
+    }
+}
+
+greeting()
+console.log(username) // username is not defined
