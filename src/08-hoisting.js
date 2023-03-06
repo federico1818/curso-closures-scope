e44badca13d03e235c83f35038d5f2222ba3948e
Federico González <federico1818@gmail.com>
2023-03-06 20:00:37 -0300

    hoisting

--- src/08-hoisting.js
+++ src/08-hoisting.js
@@ -1,3 +1,9 @@
 console.log(nameOfDog)      // undefined
 var nameOfDog = 'Firulais'
 console.log(nameOfDog)      // Firulais
+
+nameOfCat()
+function nameOfCat() {
+    console.log(`El mejor gatito es ${ cat }`)  // El mejor gatito es undefined
+}
+var cat = 'Michi'
