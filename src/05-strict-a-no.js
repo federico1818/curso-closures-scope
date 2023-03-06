2bb146be4b09d8c86971e9ea0427d030b5a029de
Federico González <federico1818@gmail.com>
2023-03-06 14:11:25 -0300

    use strict

---
+++ src/05-strict-a-no.js
@@ -0,0 +1,2 @@
+pi = 3.1416         // Asignación, no declaración
+console.log(pi)     // 3.1416
---
+++ src/05-strict-c.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    'use strict'
+    return pi = 3.1416
+}
+
+console.log(myFunction())       // pi is not defined
